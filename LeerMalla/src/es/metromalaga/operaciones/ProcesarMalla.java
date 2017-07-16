482f8655ed17a608cb4aff87cfd44d5088131925
Tracer Blaster <tracerblaster@gmail.com>
2017-07-16 13:29:24 +0200

    2017-07-16a
    - Creación de package es.metromalaga.operaciones
    - Creación de clase ProcesarMalla

---
+++ LeerMalla/src/es/metromalaga/operaciones/ProcesarMalla.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.List;
+
+import org.jdom2.Attribute;
+import org.jdom2.Content;
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+public class ProcesarMalla {
+
+	private String xmlSource = "file:///p:/Users/a.alarcon/Programas/workspace/S1_01_V01.xml";
+
+	public String getXmlSource() {
+		return xmlSource;
+	}
+
+	public void setXmlSource(String xmlSource) {
+		this.xmlSource = xmlSource;
+	}
+	
+}
